5c4fb6ab8a5f4be6e5f68a408b1123d8013259e3
Altair J. Fernandes <tatosjb@gmail.com>
2017-01-25 21:19:40 -0200

    2way working

--- app/src/main/java/com/example/altair/teste/Modelo.java
+++ app/src/main/java/com/example/altair/teste/Modelo.java
@@ -1,21 +1,26 @@
 package com.example.altair.teste;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by Altair on 25/01/2017.
  */
 
-public class Modelo {
+public class Modelo extends BaseObservable {
     private String teste;
 
     public Modelo(String teste) {
         this.teste = teste;
     }
 
+    @Bindable
     public String getTeste() {
         return teste;
     }
 
     public void setTeste(String teste) {
         this.teste = teste;
+        notifyPropertyChanged(BR.teste);
     }
 }
